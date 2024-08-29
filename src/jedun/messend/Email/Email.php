de7f60e286d77c4e5764bbbebe83add03eada0f7
Feyfa <fisikamodern00@gmail.com>
2024-08-30 06:53:50 +0700

    using guzzle client for hit my api messend, for send email

--- src/jedun/messend/Email/Email.php
+++ src/jedun/messend/Email/Email.php
@@ -2,6 +2,8 @@
 
 namespace Jidan\Jedun\Messend\Email;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
 use Rakit\Validation\Validator;
 
 require_once __DIR__ . "../../../../../vendor/autoload.php";
@@ -28,9 +30,24 @@ class Email
         $validation->validate();
 
         if($validation->fails())
-            return ['result' => 'error', 'message' => $validation->errors()];
+            return ['status' => 'error', 'message' => $validation->errors()->firstOfAll()];
         /* VALIDATOR */
 
-        return ['result' => 'success'];
+        /** SEND EMAIL */
+        $client = new Client();
+        $domain = 'https://lightgreen-salmon-353263.hostingersite.com';
+        
+        try
+        {
+            $response = $client->request('POST', "$domain/api/gmail/send", ['form_params' => $credentials]);
+            $response = json_decode($response->getBody()->getContents());
+        }
+        catch (RequestException $e)
+        {
+            return ['status' => 'error', 'message' => $e->getMessage()];
+        }
+        /** SEND EMAIL */
+
+        return ['status' => $response->status, 'message' => $response->message];
     }
 }
